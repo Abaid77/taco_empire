7b0d254391a8d7ae9452533ab811c39d25d4086c
Nahom Mehanzel <nknahom@gmail.com>
2021-08-21 18:16:17 +0000

    Add seed data for users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, phone, email, password) VALUES
+('Owner','12345678','1@example.com','password');
+('Alice','12345678','2@example.com','password');
+('Amit','12345678','3@example.com','password');
+('Nahom','12345678','4@example.com','password');
