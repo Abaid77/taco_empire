5a78391970026967b9e85053300f9dc3fc3321a8
Nahom Mehanzel <nknahom@gmail.com>
2021-08-21 19:10:33 +0000

    Seed fake data to table

--- db/seeds/02_dishes.sql
+++ db/seeds/02_dishes.sql
@@ -1,6 +1,6 @@
 -- dishes table seeds here (Example)
 INSERT INTO dishes (dish_name, description, price,image)
 VALUES
-('Beef Taco', 'Tasty taco', 5,'https://images.unsplash.com/photo-1599974579688-8dbdd335c77f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1071&q=80');
-('Chicken Taco', 'Tasty taco', 6,'https://images.unsplash.com/photo-1570461226513-e08b58a52c53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1053&q=80');
+('Beef Taco', 'Tasty taco', 5,'https://images.unsplash.com/photo-1599974579688-8dbdd335c77f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1071&q=80'),
+('Chicken Taco', 'Tasty taco', 6,'https://images.unsplash.com/photo-1570461226513-e08b58a52c53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1053&q=80'),
 ('Shrimp Taco', 'Tasty taco', 8,'https://images.unsplash.com/photo-1624300629298-e9de39c13be5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80');
