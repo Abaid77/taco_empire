58f0602942bf315d5aa399ca023c8e1b88f069ae
Nahom Mehanzel <nknahom@gmail.com>
2021-08-21 20:37:59 +0000

    Modify login routes

--- routes/login.js
+++ routes/login.js
@@ -5,17 +5,18 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     // display login page
+    res.send("Login Home Page");
   });
 
   router.get("/:user_id", (req, res) => {
-    req.session.user_id = req.params.id;
-    res.redirect('/');
+    // req.session.user_id = req.params.id;
+    res.redirect("/");
   });
   return router;
 };
