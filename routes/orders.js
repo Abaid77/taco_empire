6290dcfdabb8357ec95c8d5d9d1e9d4517061cb6
Nahom Mehanzel <nknahom@gmail.com>
2021-08-21 23:16:10 +0000

    Test order form

--- routes/orders.js
+++ routes/orders.js
@@ -5,20 +5,35 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM orders;`)
-      .then(data => {
+      .then((data) => {
         const users = data.rows;
         res.json({ users });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.post("/", (req, res) => {
+    const { user_id, dish_list } = req.body;
+    // console.log("user_id", user_id, "dish_list", dish_list);
+    db.query(
+      `INSERT INTO orders (user_id,dish_list) VALUES ($1, ARRAY [${dish_list}]);`,
+      [user_id]
+    )
+      .then((data) => {
+        const orders = data.rows;
+        console.log(orders);
+        return orders;
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
