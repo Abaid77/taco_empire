1493855590a45203f2c63afa123799f99dfd4c32
Amit Baid <baidstar@hotmail.com>
2021-08-25 09:31:35 -0600

    update shrimp taco picture

--- db/seeds/02_dishes.sql
+++ db/seeds/02_dishes.sql
@@ -3,4 +3,4 @@ INSERT INTO dishes (dish_name, description, price,image)
 VALUES
 ('Beef Taco', 'Tasty taco', 5,'https://images.unsplash.com/photo-1599974579688-8dbdd335c77f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1071&q=80'),
 ('Chicken Taco', 'Tasty taco', 6,'https://images.unsplash.com/photo-1570461226513-e08b58a52c53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1053&q=80'),
-('Shrimp Taco', 'Tasty taco', 8,'https://images.unsplash.com/photo-1624300629298-e9de39c13be5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80');
+('Shrimp Taco', 'Tasty taco', 8,'https://images.unsplash.com/photo-1611250188496-e966043a0629?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=925&q=80');
